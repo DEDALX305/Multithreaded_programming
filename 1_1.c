29b138f94c76e4610562650f7d52999c360a7343
DEDALX305 <dedalx305@mail.ru>
2016-12-08 23:13:21 +0500

    //

---
+++ 1_1.c
@@ -0,0 +1,33 @@
+﻿#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+main()
+{
+	pid_t parent = getpid();
+	pid_t p1;
+	pid_t p2;
+	printf("Оригинальная программа, pid = %d\n", parent);
+	p1 = fork();
+	if (p1 == 0)
+	{
+		printf("Я дочерний процесс 1. Мой pid = %d, родительский id = %d\n", getpid(), getppid());
+		exit(0);
+	}
+	else
+	{
+		int status;
+		waitpid(p1, &status, 0);
+		p2 = fork();
+		if (p2 == 0)
+		{
+			printf("Я дочерний процесс 2. Мой pid = %d, родительский id = %d\n", getpid(), getppid());
+			exit(0);
+		}	
+		waitpid(p2, &status, 0);
+		printf("Я родительский процесс. Hello World.\n");
+	}
+}
